f14e7888254998c5aa6c968ade90a5c67fb96eda
Kaioflores9 <kaiorodriguesrk@gmail.com>
2026-06-23 20:08:29 -0300

    Update arquivo-ox.c

--- arquivo-ox.c
+++ arquivo-ox.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int validar_caractere(char c);
-int salvar_tabuleiro(const char *nomeArquivo, char matriz[3][3]);
+#include "function_flores.c"
 
 int main(int argc, char *argv[])
 {
     char matriz[3][3];
     int i, j;
 
-    if (argc != 11)
+    if (argc != 2)
     {
-        printf("Uso: %s arquivo.txt O X O X O X O X O\n", argv[0]);
+        printf("Uso: %s arquivo.txt\n", argv[0]);
         return 1;
     }
-
-    int indice = 2;
 
     for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 3; j++)
         {
-            if (strlen(argv[indice]) != 1 ||
-                !validar_caractere(argv[indice][0]))
+            char caractere;
+
+            printf("Digite o caractere %d: ", (i * 3) + j + 1);
+            scanf(" %c", &caractere);
+
+            if (!validar_caractere(caractere))
             {
                 printf("Erro: somente O e X sao permitidos.\n");
                 return 1;
             }
 
-            matriz[i][j] = argv[indice][0];
-            indice++;
+            matriz[i][j] = caractere;
         }
     }
 
