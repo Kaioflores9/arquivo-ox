1f806969d46d30b250ea10320ff4d247ed8e44b7
Kaioflores9 <kaiorodriguesrk@gmail.com>
2026-06-23 20:10:11 -0300

    Refactor test_arquivo-ox.c with new assertions
    Updated test cases and included a new header file.

--- test_arquivo-ox.c
+++ test_arquivo-ox.c
@@ -1,17 +1,16 @@
 #include <assert.h>
 #include <stdio.h>
-#include "function_utils.h"
+#include "function_flores.c"
 
 int main(void)
 {
     assert(validar_caractere('O') == 1);
     assert(validar_caractere('X') == 1);
 
-    assert(validar_caractere('o') == 0);
-    assert(validar_caractere('x') == 0);
     assert(validar_caractere('A') == 0);
+    assert(validar_caractere('Z') == 0);
 
-    printf("Todos os testes passaram.\n");
+    printf("Teste executado com sucesso\n");
 
     return 0;
 }
